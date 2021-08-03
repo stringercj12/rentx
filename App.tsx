b1d2e9f0b50999572dbddca38fcbb09dacdae7b2
Jefferson Ferreira <jefferson14489@gmail.com>
2021-08-03 14:38:51 -0300

    Style app global, theme e fonts

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+
+import AppLoading from 'expo-app-loading';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
