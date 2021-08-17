2ceb65d360b1d75fcf6b9a56c7dc79f0fc85ec18
Jefferson Ferreira <jefferson14489@gmail.com>
2021-08-17 08:25:52 -0300

    Menu flutuante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Ionicons } from '@expo/vector-icons';
 
 import Logo from '../../assets/logo.svg';
 import { Car } from '../../components/Car';
@@ -14,8 +15,10 @@ import {
   Container,
   Header,
   HeaderContent,
-  TotalCars
+  TotalCars,
+  MyCarsButton,
 } from './styles';
+import { useTheme } from 'styled-components';
 
 export function Home() {
   const [cars, setCars] = useState<CarDTO[]>([]);
@@ -23,8 +26,14 @@ export function Home() {
 
   const navigation = useNavigation<any>();
 
+  const theme = useTheme();
+
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
+  }
+
+  function handleOpenMyCars() {
+    navigation.navigate('MyCars');
   }
 
   async function fetchCars() {
@@ -71,6 +80,13 @@ export function Home() {
         />
 
       }
+      <MyCarsButton onPress={handleOpenMyCars}>
+        <Ionicons
+          name="ios-car-sport"
+          size={RFValue(32)}
+          color={theme.colors.shape}
+        />
+      </MyCarsButton>
     </Container>
   );
 }
