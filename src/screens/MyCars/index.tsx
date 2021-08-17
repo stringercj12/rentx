0a226d9851ecafd5705ce0a5ed156746068ca146
Jefferson Ferreira <jefferson14489@gmail.com>
2021-08-17 09:05:22 -0300

    Listando carros do usuário agendado

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import { AntDesign } from '@expo/vector-icons';
+
+import { BackButton } from '../../components/BackButton';
+import { Load } from '../../components/Load';
+
+import { CarDTO } from '../../dtos/CarDTO';
+import { Car } from '../../components/Car';
+import { api } from '../../services/api';
 
 import {
-  Container
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
 } from './styles';
 
-export function MyCars(){
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO,
+  startDate: string;
+  endDate: string;
+}
+
+export function MyCars() {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+  const theme = useTheme();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  async function fetchCars() {
+    try {
+      const response = await api.get(`schedules_byuser?user_id=1`);
+      console.log(response.data);
+      setCars(response.data);
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
   return (
     <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton
+          onPress={handleBack}
+          color={theme.colors.shape}
+        />
 
+        <Title>
+          Seus agendamentos, {'\n'}
+          estão aqui.
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+
+      </Header>
+      {loading ? <Load /> :
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+
+          <FlatList
+            data={cars}
+            keyExtractor={item => item.id}
+            showVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>Período</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign
+                      name="arrowright"
+                      size={RFValue(20)}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+          />
+        </Content>
+      }
     </Container>
   );
 }
